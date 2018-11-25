9a022cad2a32b7fcc3edf86d4808138002999701
Tenorio <diegotenorio88@gmail.com>
2018-11-25 16:49:15 -0600

    MiniMax commented

--- MiniMax/MiniMax.py
+++ MiniMax/MiniMax.py
@@ -11,10 +11,11 @@ class MiniMax():
         self.current_player = current_player
         self.oponent = oponent
         
-    # state_board is object of Board class
-    # player_piece is char value   
-    # board_area is a list with the columns number to apply this
     def search_best_move(self, state_board, board_area):
+        """
+            Looks for the best move to take, it calls min_value
+            with all the columns then decides the best move amongst then
+        """
         best_move = -2
         current_max = self.MIN
         best_max = self.MIN
@@ -22,10 +23,8 @@ class MiniMax():
         for col in board_area :
             
             if not(state_board.is_column_full(col)):
-                #print("---------------------------------------",col,"---------------------------------------")
                 child = state_board.copy()
                 child.insert_value_IA(col+1, self.current_player)
-                #child.print_matrix()
                 current_max = self.min_value(0, child, self.MIN, self.MAX)
                 
                 if( current_max > best_max):
@@ -34,33 +33,38 @@ class MiniMax():
                     
         return best_move
     
-    # board is a board class 's object 
     def check_state(self, board, depth, player):
+        """
+            Returns True if theres a win, no more legal moves or the depth
+            has come to 0
+        """
         checker = Checker()
         win = checker.check_win(board, board.last_column, player)
         legal_moves = not(board.have_legal_move())
         over = depth > self.depth_max
-        #print("win: ", win, "legal moves: ", legal_moves, "over: ", over)
         return  win or legal_moves or over
             
     
     def heuristic(self, state_board):
+        """
+            Fathers Model funtion
+        """
         return 0
     
     def min_value(self, depth, state_board, alfa, beta):
+        """
+
+        """
         
         if self.check_state(state_board, depth, self.current_player):
             return self.heuristic(state_board)
             
         else:
-            #print("----------- for del min ----------")
             for col in range(state_board.column_size):
                 
                 if not(state_board.is_column_full(col)):
                     child = state_board.copy()
                     child.insert_value_IA(col+1, self.oponent) 
-                    #print("-----------jagada valida ----------")
-                    #self.print_state(depth, child)
                     temp_alfa = self.max_value(depth+1, child, alfa, beta)
                     beta = min(beta, temp_alfa)
                     if alfa >= beta:
@@ -69,20 +73,20 @@ class MiniMax():
             return beta
     
     def max_value(self, depth, state_board, alfa, beta):
+        """
+
+        """
 
         if self.check_state(state_board, depth, self.oponent):
             return self.heuristic(state_board)
             
         else: 
-            #print("----------- for del min ----------")
             for col in range(state_board.column_size):
                 
                 if not(state_board.is_column_full(col)):
                     
                     child = state_board.copy()
                     child.insert_value_IA(col+1, self.current_player)
-              #      print("-----------jagada valida ----------")
-              #      self.print_state(depth, child)
                     temp_beta = self.min_value(depth+1, child, alfa, beta)
                     alfa = max(alfa, temp_beta)
                     if alfa >= beta:
@@ -91,11 +95,20 @@ class MiniMax():
             return alfa
     
     def print_state(self, depth, board):
+        """
+            Prints the actual depth and matrix
+        """
         print ("Depth: ", depth)
         board.print_matrix()
     
 class Secuential(MiniMax):
     def heuristic(self, state_board):
+        """
+        Overwrites heuristic function, it gives a 100000 weight to 4 in line
+        discs, 100 to 3 in line discs and 1 to the 2 in line discs, then
+        returns the sum of them all, if the oponent haves 4 in line returns
+        -100000 
+        """
         checker = Secuential_Count_Checker()
         discs_4 = checker.check_lines(state_board, self.current_player, 4)
         discs_3 = checker.check_lines(state_board, self.current_player, 3)
@@ -107,6 +120,12 @@ class Secuential(MiniMax):
 
 class Espaces(MiniMax):
     def heuristic(self, state_board):
+        """
+        Overwrites heuristic function, it gives a 100000 weight to 4 in line
+        discs, 100 to 3 in line with spaces discs and 1 to the 2 in line with 
+        spaces discs, then returns the sum of them all, if the oponent haves 
+        4 in line returns -100000 
+        """
         checker = Checker()
         discs_4 = checker.check_lines(state_board, self.current_player, 4)
         discs_3 = checker.check_lines(state_board, self.current_player, 3)
