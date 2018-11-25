5eb1a2990c22fff89e9fc03271bd992b5ad1bbe9
Tenorio <diegotenorio88@gmail.com>
2018-11-25 16:19:12 -0600

    Player commented

--- Player/Player.py
+++ Player/Player.py
@@ -4,24 +4,40 @@ class Player():
     LOSSES = 1
     DRAWS = 2
 
-    # character is a char
-    # color is a string, see ConsoleControl class
     def __init__(self, character, name):
+        """
+            character is a char
+            color is a string, see ConsoleControl class
+        """
         self.character = character
         self.name = name
         self.my_pieces_positions = list()
         self.record = [0, 0, 0]
     
     def add_win(self):
+        """
+            Adds 1 to the WINS(0) in the vector record
+        """
         self.record[self.WINS] += 1
     
     def add_lose(self):
+        """
+            Adds 1 to the LOSSES(0) in the vector record
+        """
         self.record[self.LOSSES] += 1
     
     def add_draw(self):
+        """
+            Adds 1 to the DRWS(0) in the vector record
+        """
         self.record[self.DRAWS] +=1
 
     def print_record(self):
+        """
+            Prints the Record of a Player
+            e.g record=[2,1,0]
+            Player Record: Wins: 2 | Losses: 1 | draws: 0
+        """
         print("{} Record: Wins: {} | Losses: {} | draws: {}".format(self.name,
         self.record[self.WINS], self.record[self.LOSSES],
         self.record[self.DRAWS]))
