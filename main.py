9af5067b7169f983585a6a8b6311a454c54cd597
Anthony Leandro <Anthonylle@hotmail.com>
2018-11-26 23:43:47 -0600

    x2

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ def main():
     Calls the game main function(Connect4)
     return: None
     """
-    connect4 = Connect4('cls')
-    #connect4 = Connect4('clear')
+    #connect4 = Connect4('cls')
+    connect4 = Connect4('clear')
     connect4.main_menu()
 
 main()
